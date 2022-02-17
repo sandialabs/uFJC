062cd7890d7b9990122d22486503d3de439d3904
mrbuche <mrbuche@sandia.gov>
2022-02-17 16:58:05 -0700

    increment version to see it working

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 setup(
     name='ufjc',
     packages=['ufjc'],
-    version='0.2.3',
+    version='0.2.4',
     description='The Python package for the uFJC single-chain model.',
     long_description=read("README.rst"),
     author='Michael R. Buche, Scott J. Grutzik',
