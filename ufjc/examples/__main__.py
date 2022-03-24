324a98d1f09020963b2a9605d0d3672dac3fe81a
mrbuche <mrbuche@sandia.gov>
2022-03-24 08:22:15 -0600

    module-level examples running

---
+++ ufjc/examples/__main__.py
@@ -0,0 +1,15 @@
+"""Module-level functionality for running examples.
+
+Example:
+    Run the examples in the package:
+
+    ::
+
+        python -m ufjc.examples
+
+"""
+
+from ufjc.examples import isotensional_approaches
+from ufjc.examples import isotensional_asymptotics
+isotensional_approaches.main()
+isotensional_asymptotics.main(varepsilon_list=[10, 25, 100, 1000])
